6d36afaa4d80efcf894e2e50ac7c6beac9022b1d
binbin <729599457@qq.com>
2018-10-15 15:06:48 +0800

    Java API进行文件操作

--- src/test/java/com/imooc/hadoop/hdfs/HDFSApp.java
+++ src/test/java/com/imooc/hadoop/hdfs/HDFSApp.java
@@ -1,9 +1,12 @@
 package com.imooc.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.net.URI;
 
@@ -12,15 +15,36 @@ import java.net.URI;
  */
 public class HDFSApp {
 
-    public static final String HDFS_PATH="hdfs://hadoop000:8020";
+    public static final String HDFS_PATH="hdfs://localhost:9000";
 
     FileSystem fileSystem=null;
     Configuration configured=null;
 
+    /**
+     * 创建文件夹
+     * @throws Exception
+     */
+    @Test
+    public void mkdir() throws Exception{
+        fileSystem.mkdirs(new Path("/hdfsapi/test"));
+    }
+
+    /**
+     * 创建文件
+     * @throws Exception
+     */
+    @Test
+    public void create() throws Exception{
+        FSDataOutputStream outputStream =fileSystem.create(new Path("/hdfsapi/test/a.txt"));
+        outputStream.write("hello hadoop".getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
     @Before
     public void setUp()throws Exception{
         configured=new Configuration();
-        fileSystem=FileSystem.get(new URI(HDFS_PATH),configured);
+        fileSystem=FileSystem.get(new URI(HDFS_PATH),configured,"binbin");
     }
 
     @After
