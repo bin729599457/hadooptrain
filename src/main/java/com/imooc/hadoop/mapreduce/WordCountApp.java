57c009f68aa2e50cb7afb58418bd1aa9d09d0c83
binbin <729599457@qq.com>
2018-10-16 23:56:38 +0800

    读取输入的文件

--- src/main/java/com/imooc/hadoop/mapreduce/WordCountApp.java
+++ src/main/java/com/imooc/hadoop/mapreduce/WordCountApp.java
@@ -1,9 +1,24 @@
 package com.imooc.hadoop.mapreduce;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 /**
  * 使用MapReduce开发WordCount应用程序
  */
 public class WordCountApp {
 
+    /**
+     * 读取输入的文件
+     */
+    public static class MyMapper extends Mapper<LongWritable,Text,Text,LongWritable>{
 
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            super.map(key, value, context);
+        }
+    }
 }
