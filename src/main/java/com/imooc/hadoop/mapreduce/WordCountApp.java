6596957d2bd91cc139868c29598915ed74fe20c4
binbin <729599457@qq.com>
2018-10-18 11:30:33 +0800

    Map、Reduce操作

--- src/main/java/com/imooc/hadoop/mapreduce/WordCountApp.java
+++ src/main/java/com/imooc/hadoop/mapreduce/WordCountApp.java
@@ -3,6 +3,7 @@ package com.imooc.hadoop.mapreduce;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
@@ -12,13 +13,23 @@ import java.io.IOException;
 public class WordCountApp {
 
     /**
-     * 读取输入的文件
+     * Map：读取输入的文件
      */
     public static class MyMapper extends Mapper<LongWritable,Text,Text,LongWritable>{
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             super.map(key, value, context);
+        }
+    }
+
+    /**
+     * Reduce：归并操作
+     */
+    public static class MyReducer extends Reducer<Text,LongWritable,Text,LongWritable>{
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            super.reduce(key, values, context);
         }
     }
 }
